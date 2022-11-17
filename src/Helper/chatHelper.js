9ba24139e30a5f468fa522f077ea1536ecf0fb51
FlorianDray <floriandray97@gmail.com>
2022-11-17 11:13:35 +0100

    probleme hook testWorld.vue

--- src/Helper/chatHelper.js
+++ src/Helper/chatHelper.js
@@ -1,18 +1,69 @@
+
 /**
- * Reçoit le message utilisateur et remplis 
+ * retourne l'heure actuelle
+ * @returns string time
  */
-function converse(input, msgBot) {
-    if(input == '' || input == undefined) {
-        msgBot.push('Je n\'ai pas compris')
-    } else {
-        msgBot.push(chooseResponse(input)) 
+ export function getTime() {
+    let today = new Date();
+    let hours = today.getHours();
+    let minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
     }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
 }
 
+/**
+ * Reçoit le message utilisateur et remplis les listes
+ * @param input string message user
+ * @param msgBot array liste de messages du bot
+ * @returns msgBot array 
+ */
+export function converse(input, dialog) {
+    dialog.push({
+        msg: input,
+        user: 'user',
+        time : getTime()
+    })
+    if(input == '' || input == undefined) {
+        dialog.push({
+            msg: 'je n\'ai pas compris',
+            user: 'bot',
+            time : getTime()
+        })
+    } else {
+        dialog.push({
+            msg: chooseResponse(input),
+            user: 'bot',
+            time : getTime()
+        })
+    }
+    return dialog
+}
+
+/**
+ * Choisis une réponse adapté
+ * @param input string message user 
+ * @returns response string message bot
+ */
 function chooseResponse(input) {
-    
+    return input + ' bot'
 }
 
-export function firstMsgBot() {
-    
+//ça va partir dans le composant
+function generateDialog(listesMsg) {
+    listesMsg.forEach(element => {
+        if (element.index == 0) {
+            //affichage css d'un msg bot
+        } else if (element.index == 1) {
+            //affichage css d'un msg user
+        }
+    });
 }
--- src/components/Testworld.vue
+++ src/components/Testworld.vue
@@ -5,6 +5,16 @@
      <div class="chat-bar-collapsible">
         <button id="chat-button" type="button" class="collapsible"> <img class="chat-bar-icons-2" src="../assets/images/bot_logo.png"> <p id="bot-text">Chat avec Sneak Me !</p>
         </button>
+        <!-- Nouvelle partie -->
+        <div>
+            <ul>
+                 <li v-for="message in dialog">
+                    <p :class="message.user === 0 ? 'bot' : 'user'">{{ message.msg }}</p>
+                </li>
+            </ul>
+            <input type='text' v-model="inputUser">
+            <button @click="dialogEvolve()"></button>
+        </div>
 
         <div class="content">
             <div class="full-chat-block">
@@ -21,12 +31,12 @@
                         <div class="chat-bar-input-block">
                             <div id="userInput">
                                 <input id="textInput" class="input-box" type="text" name="msg"
-                                    placeholder="Écrivez un message">
+                                    placeholder="Écrivez un message" :v-model=inputUser>
                                 <p></p>
                             </div>
 
                             <div class="chat-bar-icons">
-                                <img class="chat-bar-icons" src="static/images/envoyer_shoes.png">
+                                <img class="chat-bar-icons" src="static/images/envoyer_shoes.png" @click="dialogEvolve()">
                             </div>
                         </div>
 
@@ -45,15 +55,29 @@
   </template>
   
   <script>
+    import {converse, getTime } from '../Helper/chatHelper';
     export default {
         name: 'Testworld',
         props: {
-        msg: String
+            msg: String
+        },
+        data: function() {
+            return {
+                dialog : [{
+                    msg: 'Bonjour ! Que puis-je faire pour vous ?',
+                    user: 'bot',
+                    time : getTime()
+                }],
+                inputUser: String
+            }            
         },
         created: {
-        
         },
-        methods: {}
+        methods: {
+            dialogEvolve: function () {
+                this.dialog = converse(this.inputUser, this.dialog)
+            }
+        }
     }
   </script>
 
