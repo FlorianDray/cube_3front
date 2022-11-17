15d7af46f26912f88203e9e696624777dc5fdc46
EvanHTML <evan.perroc@gmail.com>
2022-11-17 16:17:04 +0100

    MAJ HTML/CSS DIALOGUE

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,152 @@
 <template>
   <nav>
-   <!-- <router-link to="/">chat</router-link> -->
+    <!-- <router-link to="/">chat</router-link> -->
   </nav>
-  <router-view/>
+  <router-view />
 
 
 </template>
 
 <style>
-
 body {
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
+  background-color: rgb(47, 47, 47);
 }
 
-.chatbot {
+.chatbot-box {
+  position: fixed;
+  display: grid;
+  bottom: 0;
+  right: 50px;
+}
 
+.button-open-chatbox {
+  position: absolute;
+  background-color: #0A7CFF;
+  width: 350px;
+  height: 50px;
+  font-size: 18px;
+  bottom: 500px;
+  right: 0;
+  outline: none;
+  cursor: pointer;
+  border-radius: 20px 20px 0px 0px;
+  border: 3px solid white;
+  border-bottom: none;
+  box-shadow: 0 8px 16px 0 rgb(0, 0, 0);
+}
+
+#bot-text {
+  text-align: left;
+  vertical-align: middle;
+  padding: 15px;
+  margin: 0;
+  color: white;
+}
+
+.button-open-chatbox img {
+  justify-content: space-evenly;
+  box-sizing: border-box;
+  width: 50px;
+  float: left;
 }
 
 .windows-chatbot {
-  background-color: antiquewhite;
+  background-color: white;
   max-height: 500px;
   width: 350px;
   position: absolute;
+  display: flex;
+  flex-direction: column;
   bottom: 0;
   right: 0;
   scroll-behavior: smooth;
   hyphens: auto;
+  
+
 }
-.windows-chatbot::-webkit-scrollbar {
-    display: none;
+
+.scrollbar {
+  overflow: auto;
+  scrollbar-width: none;
+  height: max-content;
+  transition: max-height 0.2s ease-out;
 }
 
 .windows-chatbot ul li {
   list-style-type: none;
+  text-align: left;
+  padding:0;
+  margin: 0;
+}
+
+.text-zone {
+  background-color: rgb(216, 216, 216);
+  padding: 10px;
 }
 
 .windows-chatbot button {
-  width: 50px;
-  height: 20px;
+  width: 30%;
+  height: 25px;
+  background: rgb(216, 216, 216);
+  border: 0;
 }
 
+.image-envoyer {
+  display: flex;
+  justify-content: space-evenly;
+  box-sizing: border-box;
+  width: 50px;
+  float: right;
+  margin-right: 20px;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.chat-textbox {
+  display: flex;
+  float: left;
+  box-sizing: border-box;
+  justify-content: space-between;
+  width: 70%;
+  align-items: center;
+  background-color: rgb(255, 255, 255);
+  border-radius: 5px;
+  padding: 10px;
+  border: 0;
+  outline: none;
+}
+
+/* .userBox {
+  line-height: 1.5em;
+  display: inline-block;
+  background: #e0e0e0;
+  padding: 10px;
+  border-radius: 8px;
+  border-bottom-left-radius: 2px;
+  max-width: 75%;
+  margin-left: 10px;
+} */
+
+.user {
+  padding: 20px;
+  color: white;
+  background: linear-gradient(120deg, rgba(97,33,158,1) 15%, rgba(9,9,121,1) 50%, rgba(0,138,166,1) 100%);    
+  width: 75%;
+  float: right;
+  margin-right: 10px;
+  border-radius: 10px;
+  max-width: max-content;
+}
+
+.bot {
+  padding: 20px;
+  color: rgb(0, 0, 0);
+  background: #e0e0e0;
+  width: 75%;
+  float: left;
+  margin-left: -30px;
+  border-radius: 10px;
+}
 </style>
--- src/components/ChatComponent.vue
+++ src/components/ChatComponent.vue
@@ -1,39 +1,53 @@
 <template>
-    <div class="chatbot">
-      <h1>{{ msg }}</h1>
+  <div class="page-chatbot">
+    <h1>{{ msg }}</h1>
+    <div class="chatbot-box">
+      <div id="button-chatbox">
+        <button type="button" class="button-open-chatbox"> <img class="chat-bar-icons-2"
+            src="../assets/images/bot_logo.png">
+          <p id="bot-text">Chat avec Sneak Me !</p>
+        </button>
+
+      </div>
       <div class="windows-chatbot">
-            <ul>
-                 <li v-for="message in dialog">
-                    <p :class="message.user === 0 ? 'bot' : 'user'">{{ message.msg }}</p>
-                </li>
-            </ul>
-            <input type='text' v-model="inputUser">
-            <button @click="dialogEvolve()"></button>
+        <ul class="scrollbar">
+          <li v-for="message in dialog">
+            <div class="userBox">
+              <p :class="message.user == 'bot' ? 'bot' : 'user'">{{ message.msg }}</p>
+            </div>
+          </li>
+        </ul>
+        <div class="text-zone">
+          <input type='text' class="chat-textbox" placeholder="Écrivez un message" v-model="inputUser">
+          <button @click="dialogEvolve()"><img src="../assets/images/envoyer_shoes.png" class="image-envoyer"></button>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
-  <script>
-    import {converse} from '../helpers/chatHelper';
-    export default {
-        name: 'Testworld',
-        props: {
-            msg: String
-        },
-        data() {
-          return {
-            dialog: [{
-              msg: 'Bonjour ! comment souhaitez vous recherchez ? (marque/type/nom/couleur/taille)',
-              user: 'bot'
-            }],
-            inputUser: ""
-          }
-        },
-        methods: {
-            dialogEvolve() {
-                this.dialog = converse(this.inputUser, this.dialog)
-            }
-        }
+<script>
+import { converse, getTime } from '../Helper/chatHelper';
+export default {
+  name: 'Testworld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      dialog: [{
+        msg: 'Bonjour ! Que puis-je faire pour vous ?',
+        user: 'bot',
+        time: ""
+      }],
+      inputUser: "Coucou"
     }
-  </script>
+  },
+  methods: {
+    dialogEvolve() {
+      this.dialog = converse(this.inputUser, this.dialog)
+    }
+  }
+}
+</script>
 
